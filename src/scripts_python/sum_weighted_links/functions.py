c251258f290075c097fb9017d3316d911da024e7
csandorfr <csandorfr@gmail.com>
2016-02-08 14:10:18 +0000

    Removed src from repository

--- src/scripts_python/sum_weighted_links/functions.py
+++
@@ -1,8 +0,0 @@
-def weighted_sum_marcott(list_score,m):
-        sum_link=0
-        for i in range(0,len(list_score)):
-                if i==0:
-                        sum_link+=list_score[i]
-                else:
-                        sum_link+=list_score[i]/(float(m*i))
-        return sum_link
